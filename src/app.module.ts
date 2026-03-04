004f49a4ac9a70ffdeef0aed22f18f7bfe60ae0d
AzarRaphiyev <tu6zbxap5@code.edu.az>
2026-03-04 15:47:10 +0400

    deyisiklikler 2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { ProductsModule } from './modules/products/products.module';
+import { SalesModule } from './modules/sales/sales.module';
+import { InventoryModule } from './modules/inventory/inventory.module';
+import { ReturnsModule } from './modules/returns/returns.module';
+import { ParametersModule } from './modules/parameters/parameters.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, AuthModule, ProductsModule, SalesModule, InventoryModule, ReturnsModule, ParametersModule],
   controllers: [AppController],
   providers: [AppService],
 })
